f4ecd426de0a37a43221f9a5ee80480126b1eb11
Tomas <tomaserobino@gmail.com>
2024-08-20 20:01:25 +0200

    parsing data from response done

--- src/Countries.tsx
+++ src/Countries.tsx
@@ -1,21 +1,24 @@
 import styles from './Countries.module.css'
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import CountryElement from "./CountryElement.tsx";
+import {data} from "./types.tsx";
 
 export default function Countries(props: {searchText: string}) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([] as Array<data>);
 
   useEffect(() => {
     if (props.searchText === "") {
       fetch("https://restcountries.com/v3.1/all")
         .then(response => response.json())
-        .then(data => console.log(data));
     } else {
       fetch(`https://restcountries.com/v3.1/name/${props.searchText}`)
-       .then(response => response.json())
-       .then(data => setData(data));
+        .then(response => response.json())
+        .then(data => setData(data))
     }
   }, [props.searchText])
   return(
-    <></>
+    <>
+      {props.searchText !== "" && data[0] !== undefined ? <CountryElement data={data[0]}/> : <></> }
+    </>
   )
 }
--- src/CountryElement.tsx
+++ src/CountryElement.tsx
@@ -2,12 +2,10 @@ import styles from './CountryElement.module.css'
 import {data} from "./types.tsx";
 
 export default function CountryElement(props: { data: data }) {
-  //props.data is json
-
   return(
     <div>
-      <h2>{props.data.name.nativeName.eng.official}</h2>
-      <p>{props.data.capital? props.data.capital[0] + " " + props.data.capital[1] : ""}</p>
+      <p>{Object.values(props.data.name)[0]}</p>
+      <p>{props.data.capital? props.data.capital : ""}</p>
       <img className={styles.flag} src={props.data.flag} alt={props.data.flags.alt} />
       <p>{props.data.population}</p>
       <p>{props.data.region}</p>
