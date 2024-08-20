c406a7ef7a503bc8633cff2f65190aae6a5014a7
Tomas <tomaserobino@gmail.com>
2024-08-20 20:09:02 +0200

    fixed flag url

--- src/CountryElement.tsx
+++ src/CountryElement.tsx
@@ -6,7 +6,7 @@ export default function CountryElement(props: { data: data }) {
     <div>
       <p>{Object.values(props.data.name)[0]}</p>
       <p>{props.data.capital? props.data.capital : ""}</p>
-      <img className={styles.flag} src={props.data.flag} alt={props.data.flags.alt} />
+      <img className={styles.flag} src={props.data.flags.svg} alt={props.data.flags.alt} />
       <p>{props.data.population}</p>
       <p>{props.data.region}</p>
       <p>{props.data.area}</p>
