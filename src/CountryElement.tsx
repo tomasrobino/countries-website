5c8db3568d7790a59721573261fb3751efd8c2ff
Tomas <tomaserobino@gmail.com>
2024-08-20 19:07:06 +0200

    basic CountryElement structure

--- src/CountryElement.tsx
+++ src/CountryElement.tsx
@@ -1,19 +1,17 @@
 import styles from './CountryElement.module.css'
+import {data} from "./types.tsx";
 
-export default function CountryElement(props: { data: any }) {
+export default function CountryElement(props: { data: data }) {
   //props.data is json
 
   return(
     <div>
-      <h2>{props.data.name}</h2>
-      <p>{...props.data.capital}</p>
-      <img className={styles.flag} src={props.data.flag} alt={props.data.name} />
+      <h2>{props.data.name.nativeName.eng.official}</h2>
+      <p>{props.data.capital? props.data.capital[0] + " " + props.data.capital[1] : ""}</p>
+      <img className={styles.flag} src={props.data.flag} alt={props.data.flags.alt} />
       <p>{props.data.population}</p>
       <p>{props.data.region}</p>
-      <p>{props.data.subregion}</p>
       <p>{props.data.area}</p>
-      <p>{props.data.gini}</p>
-      <p>{props.data.borders.join(', ')}</p>
     </div>
   )
 }
