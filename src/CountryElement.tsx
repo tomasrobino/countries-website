01117faca2dbf902d2dbf01529d13256bdaccb07
Tomas <tomaserobino@gmail.com>
2024-08-21 23:00:07 +0200

    styling for card

--- src/CountryElement.tsx
+++ src/CountryElement.tsx
@@ -17,7 +17,7 @@ export default function CountryElement(props: { data: data }) {
     <div className={`${styles.card} ${hover ? styles.cardHover : null}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className={`${styles.titleDiv} ${hover ? null: styles.divNotHover}`}>
         <p>{Object.values(props.data.name)[0]}</p>
-        <p>{props.data.capital ? props.data.capital : ""}</p>
+        <p>{props.data.capital ? props.data.capital : "Unknown capital"}</p>
       </div>
       <img className={styles.flag} src={props.data.flags.svg} alt={props.data.flags.alt} />
       <div className={`${styles.dataDiv} ${hover ? null : styles.divNotHover}`}>
